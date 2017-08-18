0e701da4de8f9be53ce7fc1124be2557a1c082dc
Manokero <dancebreak29@gmail.com>
2017-08-17 20:26:19 -0400

    Bootstrap y Jquery, configurados y agragados

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css',express.static(path.join(__dirname,
+	 'node_modules/bootstrap/dist/css/')))
 
+//Routes
 app.use('/', index);
 app.use('/users', users);
 

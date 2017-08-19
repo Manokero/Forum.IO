9188fd9e31b24a356fc424b955007f32f3e4c8b9
Manokero <dancebreak29@gmail.com>
2017-08-19 01:15:07 -0400

    deploy to heroku

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',function(req,res){
+ console.log("hola")
+})
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
 });
+
+router.get('/signin',function(req,res){
+	res.send("NOT STILL AVALIBLE")
+})
 
 module.exports = router;

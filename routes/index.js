c39ba7c953ca4f31baa97c70acec210f2207776a
Manokero <dancebreak29@gmail.com>
2017-08-19 18:44:31 -0400

    Viste fe signup mejorada, index agregado y algunas funciones de rutas cambiadas

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',function(req,res){
- console.log("hola")
+ res.render('index')
 })
 
 router.get('/signup', function(req, res, next) {
   res.render('signup');
 });
 
+router.post('/signup',function(req,res){
+	
+})
+
 router.get('/signin',function(req,res){
 	res.send("NOT STILL AVALIBLE")
 })
+
+
 
 module.exports = router;
